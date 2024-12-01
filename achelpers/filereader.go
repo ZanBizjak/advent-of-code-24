06cc984d2f0ff7b28b42be0c82ad8775a1962ecd
Zan Bizjak <btz.bizi@gmail.com>
2024-12-01 13:50:32 +0100

    Day1 task1

--- achelpers/filereader.go
+++ achelpers/filereader.go
@@ -1,17 +1,53 @@
 package achelpers
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 func StringReadFile(file string) string {
 	dat, err := os.ReadFile(file)
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return string(dat)
+}
+
+func IntReadTwoColumns(filestring string, splitter string) ([]int, []int) {
+	file, err := os.Open(filestring)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	col1 := make([]int, 1000)
+	col2 := make([]int, 1000)
+
+	for scanner.Scan() {
+		row := scanner.Text()
+		fmt.Println(row)
+		firstNum, err := strconv.Atoi(strings.Split(row, splitter)[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		secondNum, err := strconv.Atoi(strings.Split(row, splitter)[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		col1 = append(col1, firstNum)
+		col2 = append(col2, secondNum)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return col1, col2
 }
--- day1/day1.go
+++ day1/day1.go
@@ -2,11 +2,25 @@ package day1
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/zanBizjak/advent-of-code-24/achelpers"
 )
 
 func Task1() {
-	fmt.Println(achelpers.StringReadFile("day1.txt"))
+	col1, col2 := achelpers.IntReadTwoColumns("day1.txt", "   ")
+	sort.Slice(col1, func(i, j int) bool { return col1[i] > col1[j] })
+	sort.Slice(col2, func(i, j int) bool { return col2[i] > col2[j] })
 
+	sum := 0
+	for i := 0; i < len(col1); i++ {
+		presum := col1[i] - col2[i]
+		if presum < 0 {
+			presum *= -1
+		}
+		sum += presum
+
+	}
+
+	fmt.Println(sum)
 }
