b23a9e8b9ceb44918e7f0bc25cc2754e5a654394
Zan Bizjak <btz.bizi@gmail.com>
2024-12-05 17:44:31 +0100

    day5task2

--- day5/day5.go
+++ day5/day5.go
@@ -51,4 +51,53 @@ func inOrder(rules map[int]map[int]bool, update string) int {
 }
 
 func Task2() {
+	before, after := achelpers.IntReadTwoColumns("day5rules.txt", "|")
+	rules := make(map[int]map[int]bool, 0)
+	for i, rule := range before {
+		if rules[rule] == nil {
+			rules[rule] = make(map[int]bool)
+		}
+		rules[rule][after[i]] = true
+	}
+
+	updates := achelpers.ReadRows("day5.txt")
+	res := 0
+	for _, update := range updates {
+		if inOrder(rules, update) == 0 {
+			res += findOrder(rules, update)
+		}
+
+	}
+
+	fmt.Println(res)
+}
+
+func findOrder(rules map[int]map[int]bool, update string) int {
+	updateArr := strings.Split(update, ",")
+	slices.Reverse(updateArr)
+	var updateInt []int
+	for _, num := range updateArr {
+		i, _ := strconv.Atoi(num)
+		updateInt = append(updateInt, i)
+	}
+	updateInt = findOrderRec(rules, updateInt)
+
+	return updateInt[len(updateInt)/2]
+
+}
+
+func findOrderRec(rules map[int]map[int]bool, updateInt []int) []int {
+	for i := 0; i < len(updateInt)-1; i++ {
+		page := updateInt[i]
+		for j := i + 1; j < len(updateInt); j++ {
+			if rules[page][updateInt[j]] {
+				switcheroo := updateInt[j]
+				updateInt[j] = updateInt[i]
+				updateInt[i] = switcheroo
+				return findOrderRec(rules, updateInt)
+			}
+
+		}
+	}
+	return updateInt
 }
