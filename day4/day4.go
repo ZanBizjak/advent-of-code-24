f5ef402ee24c2fdc9f8bfd2bc2cc5b2bfa438185
ZanBizjak <32543948+ZanBizjak@users.noreply.github.com>
2024-12-05 06:40:02 +0100

    day4task1and2

--- day4/day4.go
+++ day4/day4.go
@@ -10,40 +10,39 @@ import (
 func Task1() {
 	grid := achelpers.RuneGetGrid("day4.txt")
 	numOfXmas := 0
-	for _, words := range grid {
-		numOfXmas += findXmas(words)
-	}
-
-	for _, words := range grid {
-		reversed := achelpers.RuneCopySlice(words)
-		slices.Reverse(reversed)
-		numOfXmas += findXmas(words)
-	}
-
-	for i := len(grid) - 1; i >= 0; i-- {
+	for i := 0; i < len(grid); i++ {
 		words := getDiagonal(grid, i, 0, 1, 1)
 		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, 0, i, 1, 1)
+		slices.Reverse(words)
 		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, len(grid)-1, i, -1, -1)
-		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, i, len(grid)-1, -1, -1)
-		numOfXmas += findXmas(words)
+		if i != 0 {
+			words = getDiagonal(grid, 0, i, 1, 1)
+			numOfXmas += findXmas(words)
+			slices.Reverse(words)
+			numOfXmas += findXmas(words)
+		}
 
 		words = getDiagonal(grid, len(grid)-1, i, -1, 1)
 		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, i, len(grid)-1, -1, 1)
+		slices.Reverse(words)
+		numOfXmas += findXmas(words)
+		if i != len(grid)-1 {
+			words = getDiagonal(grid, i, 0, -1, 1)
+			numOfXmas += findXmas(words)
+			slices.Reverse(words)
+			numOfXmas += findXmas(words)
+		}
+
+		words = getDiagonal(grid, i, 0, 0, 1)
+		numOfXmas += findXmas(words)
+		slices.Reverse(words)
 		numOfXmas += findXmas(words)
 
 		words = getDiagonal(grid, 0, i, 1, 0)
 		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, len(grid)-1, i, -1, 0)
+		slices.Reverse(words)
 		numOfXmas += findXmas(words)
 
-		words = getDiagonal(grid, i, 0, 0, 1)
-		numOfXmas += findXmas(words)
-		words = getDiagonal(grid, i, len(grid)-1, 0, -1)
-		numOfXmas += findXmas(words)
 	}
 
 	fmt.Println(numOfXmas)
@@ -57,6 +56,7 @@ func getDiagonal(grid [][]rune, startY, startX, dirY, dirX int) []rune {
 
 	for yPos < len(grid) && yPos >= 0 && xPos < len(grid) && xPos >= 0 {
 		word = append(word, grid[yPos][xPos])
+		fmt.Print(string(grid[yPos][xPos]))
 		xPos += dirX
 		yPos += dirY
 
@@ -72,29 +72,41 @@ func findXmas(r []rune) int {
 		switch runeState {
 		case 0:
 			if char == 'X' {
-				runeState++
+				runeState += 1
 				break
 			}
 			runeState = 0
 		case 1:
 			if char == 'M' {
-				runeState++
+				runeState += 1
 				break
 			}
-			runeState = 0
+			if char == 'X' {
+				runeState = 1
+			} else {
+				runeState = 0
+			}
 
 		case 2:
 			if char == 'A' {
-				runeState++
+				runeState += 1
 				break
 			}
-			runeState = 0
+			if char == 'X' {
+				runeState = 1
+			} else {
+				runeState = 0
+			}
 		case 3:
 			if char == 'S' {
-				runeState++
+				runeState += 1
 				break
 			}
-			runeState = 0
+			if char == 'X' {
+				runeState = 1
+			} else {
+				runeState = 0
+			}
 		}
 		if runeState == 4 {
 			numOfXmas++
@@ -107,4 +119,20 @@ func findXmas(r []rune) int {
 }
 
 func Task2() {
+	grid := achelpers.RuneGetGrid("day4.txt")
+	numOfMas := 0
+	for i := 1; i < len(grid)-1; i++ {
+		for j := 1; j < len(grid)-1; j++ {
+			if grid[i][j] == 'A' {
+				if (grid[i-1][j-1] == 'M' && grid[i+1][j+1] == 'S') || (grid[i-1][j-1] == 'S' && grid[i+1][j+1] == 'M') {
+
+					if (grid[i+1][j-1] == 'M' && grid[i-1][j+1] == 'S') || (grid[i+1][j-1] == 'S' && grid[i-1][j+1] == 'M') {
+						numOfMas++
+					}
+				}
+			}
+
+		}
+	}
+	fmt.Println(numOfMas)
 }
